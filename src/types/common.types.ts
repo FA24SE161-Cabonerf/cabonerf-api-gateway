80a0a7aea8559b40891e0e2ee8d35205bf752b70
13gucci <bibabibu0110@gmail.com>
2024-10-22 13:03:21 +0700

    CABONERF-102: migrate api get me from main-be to gateway

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -3,3 +3,15 @@ export interface CommonResponse<T> {
 	message: string;
 	data: T;
 }
+
+export class GatewayResponse<T> {
+	status: number;
+	message: string;
+	data: T;
+
+	constructor({ status, message, data }: { status: number; message: string; data: T }) {
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
+}
