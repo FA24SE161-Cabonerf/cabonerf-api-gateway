f7aed7a2e42bef6a1757756111ae18ea06d035d1
phhs <phanson03vt@gmail.com>
2024-11-26 17:51:06 +0700

    fix import exce,

--- src/controllers/cabonerf-main/midpointFactor.controller.ts
+++ src/controllers/cabonerf-main/midpointFactor.controller.ts
@@ -47,8 +47,8 @@ class MidpointFactorController {
 		result.data.pipe(res);
 	}
 
-	public async importExcel(_req: Request, res: Response) {
-		const { methodName } = _req.body;
+	public async importExcel(_req: Request<ParamsDictionary, any, any, { methodName: string }>, res: Response) {
+		const { methodName } = _req.query;
 		const file = _req.file;
 		if (!file) {
 			return res.status(StatusCodes.BAD_REQUEST).json(
