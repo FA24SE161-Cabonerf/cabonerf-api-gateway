c81919da27b888f7df26c01295d769cb91903a5b
phhs <phanson03vt@gmail.com>
2024-12-09 23:44:02 +0700

    dataset service

--- src/services/cabonerf-main/dataset.service.ts
+++ src/services/cabonerf-main/dataset.service.ts
@@ -4,7 +4,9 @@ import mainAxiosService from './main.axios';
 export default class DatasetService {
 	public async getAllDataset(projectId: string) {
 		return mainAxiosService.axios.get(ROUTE_ENDPOINTS.DATASETS, {
-			params: projectId
+			params: {
+				projectId: projectId
+			}
 		});
 	}
 }
